4563dc67dacbf7cddf4543e401bad8e95c4d71a6
NenadMerc <nenad.bobic@mercator.rs>
2022-07-27 13:47:04 +0200

    Added interface folder.

---
+++ KGB_Dev_/Interfaces/IDataRetrivingServices.cs
@@ -0,0 +1,22 @@
+﻿using KGB_Dev_.Data.KGB_Model;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace KGB_Dev_.Interfaces
+{
+    public interface IDataRetrivingServices
+    {
+        Task<List<KGB_Knowledge>> GetListOfKnowledge(int OrgJed);
+        Task<List<KGB_Knowledge>> GetPublicListOfKnowledge();
+        Task<KGB_Knowledge> GetKnowledge(long id);
+        Task<KGB_User> GetCurrentUser();
+        Task<List<KGB_Category>> GetCategory();
+        Task<List<string>> GetFile(string Path);
+        Task NavigationManager(string nav);
+        Task<List<KGB_Subcategory>> GetSubcategory(int category_id);
+        Task<List<KGB_Subcategory>> GetSubcategory();
+        Task<Dictionary<string, string>> GetUsersFromOj(int SifraOj);
+        Task<string> UploadFile(string NazivPrijave, IList<IBrowserFile> ListOfFile);
+        Task<List<KGB_Oj>> GetListOfOrgJed();
+
+    }
+}
